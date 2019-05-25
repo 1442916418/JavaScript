ea36dcd62814f043c5ff6bfdf576723d20c9aa75
Yss <1442916418@qq.com>
2019-05-25 09:40:02 +0800

    JavaScript 图片放大功能

---
+++ PictureEnlargement/js/index.js
@@ -0,0 +1,29 @@
+
+function modalFramePicture() {
+    var body = document.getElementById('body');
+    var imgAll = body.getElementsByTagName('img');
+
+    for (var i = 0; i < imgAll.length; i++) {
+        imgAll[i].index = i;
+        imgAll[i].onclick = function () {
+            var createMask = document.createElement('div');
+            createMask.className = 'createMask';
+            createMask.addEventListener('click', function () {
+                body.removeChild(createMask);
+            });
+
+            var createImg = document.createElement('img');
+            var currentPicture = imgAll[this.index];
+            createImg.className = 'createImg'
+            createImg.src = currentPicture.src;
+            createImg.style.width = currentPicture.width * 2 + 'px';
+            createImg.style.height = currentPicture.height * 1.5 + 'px';
+            createImg.className = ' TopToBottom';
+
+            createMask.appendChild(createImg);
+            body.appendChild(createMask);
+        }
+    }
+}
+
+modalFramePicture();
